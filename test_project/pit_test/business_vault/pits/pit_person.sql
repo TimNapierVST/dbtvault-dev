a97a06bc2d5d5ebc79eaf843bad1030c1f2b62c4
TimNapier <tim.napier@hotmail.com>
2021-01-06 14:39:23 +1100

    Added pit collateral
    Added PIT Macro, dbt_project.yml example, pit_person.sql example

---
+++ test_project/pit_test/business_vault/pits/pit_person.sql
@@ -0,0 +1,11 @@
+{{ dbtvault.pit(
+    var('satellites'),
+    var('src_pk'), 
+    var('src_hashdiff'),     
+    var('src_eff'),
+    var('src_ldts'),
+    var('src_source'),
+    var('src_db')
+
+    )
+}}
